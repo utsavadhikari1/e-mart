801f58998fde2a8174b2587e21c90eb8be52d5ed
unknown <you@example.com>
2023-08-04 21:28:42 +0545

    Cahnge in Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,70 +1,52 @@
 import React from "react";
-import { fontAwesomeIcon } from "react-fontawesome ";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="sb_footer section_padding">
-        <div className="sb_footer-links">
-          <div className="sb_footer-links-div">
-            <h4>For Business</h4>
-            <a href="/employer">
-              <p>Employer</p>
-            </a>
-            <a href="/healthplan">
-              <p>Health Plan</p>
-            </a>
-            <a href="/individual">
-              <p>Individual</p>
-            </a>
-          </div>
-          <div className="sb_fotter-links_div">
-            <h4>Resoures</h4>
-            <a href="/resource">
-              <p>Resource Center</p>
-            </a>
-            <a href="/resource">
-              <p>Testimonials</p>
-            </a>
-            <a href="/resource">
-              <p>STV</p>
-            </a>
-          </div>
-          <div className="sb_footer-links_div">
-            <h4>Patners</h4>
-            <a href="/employer">
-              <p>Swing Tech</p>
-            </a>
-          </div>
-          <div className="sb_footer-links-div">
-            <h4>Company</h4>
-            <a href="/about">
-              <p>About</p>
-            </a>
-            <a href="/press">
-              <p>Press</p>
-            </a>
-            <a href="/career">
-              <p>Career</p>
-            </a>
-            <a href="/contact">
-              <p>Contact</p>
-            </a>
-          </div>
-          <div className="sb_footer-links-div">
-            <h4>Coming Soon</h4>
-            <div className="socialMedia">
-              <p>
-                <img src={"fa-brands fa-facebook"} alt="" />
-              </p>
-              <p>
-                <img src={"fa-sharp fa-light fa-envelope"} alt="" />
-              </p>
-              <p>
-                <img src={"fa-brands fa-telegram"} alt="" />
-              </p>
+    <div className="text-white bg-black">
+      <div className="row">
+        <div class="col-md-3">
+          <h1 className="font-weight-bold mt-5 pt-2 font-size-70px">
+            <h2>NP COLLECTION</h2>
+            <div className="sb_footer-below">
+              <div className="sb_footer-copyright">
+                <p>
+                  <h6>
+                    @{new Date().getFullYear()} NP COLLECTION. All right
+                    reserved.
+                  </h6>
+                </p>
+              </div>
             </div>
-          </div>
+            {/* <div className="flex">
+              <img
+                className="logo"
+                alt="menu"
+                src="https://t4.ftcdn.net/jpg/04/71/27/39/360_F_471273924_csGsGkwKPvSRa0ppdt0KX0pzy02BYKwe.jpg"
+              />
+            </div> */}
+          </h1>
+        </div>
+
+        <div className="col-md-3">
+          <h4 className="font-weight-bold mb-3">Company</h4>{" "}
+          <hr className="bg-white h-2 w-50% m-l-0" />
+          <p>PRODUCTIONS</p>
+          <p>FIND STORE</p>
+          <p>SEND US FEEDBACK</p>
+        </div>
+
+        <div className="col-md-3">
+          <h4 className="font-weight-bold mb-3">MY ACCOUNT</h4> <hr />
+          <p>Login</p>
+          <p>CONTACT US</p>
+          <p>BE A MEMBER</p>
+        </div>
+
+        <div className="col-md-3">
+          <h4 className="font-weight-bold mb-3">ABOUT US </h4> <hr />
+          <p>NEWS</p>
+          <p>SIGN UP FOR MAIL</p>
+          <p>SHIPPING & DELIVERY</p>
         </div>
       </div>
     </div>
